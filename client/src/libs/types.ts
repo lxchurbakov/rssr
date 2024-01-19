4d7b8c014bb199f06b52c9195062d4ef97d6849e
lxchurbakov <lxchurbakov@gmail.com>
2024-01-19 17:07:41 +0300

    Add types libs and start adding types everywhere

--- client/src/libs/types.ts
+++ client/src/libs/types.ts
@@ -1,2 +1,18 @@
 export type Predicate<A extends any[], R> = (...args: A) => R;
 export type ArgumentsOf<T> = T extends Predicate<infer A, any> ? A : never;
+
+export type Entity<T> = T & { _id: string };
+export type Tag = Entity<{ name: string }>;
+
+export type Option = { value: string, label: string };
+
+export type Input<T> = { value: T, onChange: ($: T) => void };
+
+export type Post = Entity<{
+    title: string;
+    preview: string;
+    pubDate: Date;
+    link: string;
+    views: number;
+    tags: string[];
+}>;
